71d8ba0fee86fcabc4e7d43f9c85e8482cbf46bd
Basil Aslam <basilaslamnp@gmail.com>
2023-07-14 22:04:47 +0400

    created home module and home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AuthModule } from './auth/auth.module';
+import { HomeModule } from './home/home.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -15,6 +16,7 @@ import { AppComponent } from './app.component';
     AppRoutingModule,
     AuthModule,
     HttpClientModule,
+    HomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
